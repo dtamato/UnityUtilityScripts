a4fb81aa9bf74790659097c6401f396c5f7385fa
Dave Tamayo <djbtamayo@gmail.com>
2016-07-29 15:57:59 -0400

    Added in a script to handle scheduling local notifications on iOS devices.

---
+++ LocalNotification.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using NotificationServices = UnityEngine.iOS.NotificationServices;
+using NotificationType = UnityEngine.iOS.NotificationType;
+
+public class LocalNotification : MonoBehaviour {
+    
+    public static void ScheduleNotificationForiOSWithMessage (string text, System.DateTime fireDate) {
+        
+        if (Application.platform == RuntimePlatform.IPhonePlayer) {
+            
+            UnityEngine.iOS.LocalNotification notification = new UnityEngine.iOS.LocalNotification ();
+            notification.fireDate = fireDate;
+            notification.alertAction = "Alert";
+            notification.alertBody = text;
+            notification.hasAction = false;
+
+            NotificationServices.ScheduleLocalNotification (notification);
+
+            #if UNITY_IOS
+            NotificationServices.RegisterForNotifications(NotificationType.Alert | NotificationType.Badge | NotificationType.Sound);
+            #endif
+        }        
+    }
+}
