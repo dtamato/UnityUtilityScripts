004be8b2d85052136c05a6ab444f98a4ddcce468
Dave Tamayo <djbtamayo@gmail.com>
2016-08-22 16:55:42 -0400

    ToggleGameobject

---
+++ ToggleGameobject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleGameobject : MonoBehaviour {
+
+    public void ToggleObject (GameObject objectToToggle) {
+
+        objectToToggle.SetActive(!objectToToggle.activeSelf);
+    }
+}
