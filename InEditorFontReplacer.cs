5bf101426dd89b295d2bb7b6946f39b55ef5b04a
Dave Tamayo <djbtamayo@gmail.com>
2016-12-03 20:44:33 -0500

    InEditorFontReplacer:  Replaces the fonts of all Text objects in a scene, in editor

---
+++ InEditorFontReplacer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class InEditorFontReplacer : MonoBehaviour {
+
+	[SerializeField] Font newFont;
+
+	void Start () {
+	
+		if(newFont) {
+
+			Text[] allTextObjects = GameObject.FindObjectOfType<Canvas>().GetComponentsInChildren<Text>();
+			foreach (Text textObject in allTextObjects) {
+
+				textObject.font = newFont;
+			}
+		}
+	}
+}
